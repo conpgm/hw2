eaa5c68bac73126458979368da7136eb850e9396
Weiming Wu <tianyayiren0122@gmail.com>
2013-11-26 21:54:01 +0200

    improve concurrency succefully

--- tuplespace/tuplespaces/LocalTupleSpace.java
+++ tuplespace/tuplespaces/LocalTupleSpace.java
@@ -6,57 +6,104 @@ public class LocalTupleSpace implements TupleSpace {
 	
 	ArrayList<String[]> space;
 	ArrayList<String[]> waiting;
-	
+//	int count =0;
 	public LocalTupleSpace () {
 		space = new ArrayList<String[]>();
 		waiting = new ArrayList<String[]>();
 	}
 	
+//	public synchronized void printPattern(String[] pattern, String name, int myCount){
+//		System.out.print("count="+myCount+"  "+name+": ");
+//		for(String item : pattern){
+//			System.out.print(item);
+//		}
+//		System.out.println();
+//	}
+	
+//	public synchronized void printSpace(String name, int myCount){
+//		System.out.println("***************************");
+//		for(String[] pattern : this.space){
+//			this.printPattern(pattern, name, myCount);
+//		}
+//		System.out.println("***************************");
+//	}
+	
 	public String[] get(String... pattern) {
 		String[] tp;
 		String[] wp = addWaiting(pattern);
-		
-		while ((tp = search(pattern)) == null) {
-			synchronized (wp) {
+//		int myCount;
+//		synchronized(this){
+//			count++;
+//			myCount=count;
+//		}
+		synchronized(wp){
+			while ((tp = search(pattern)) == null) {
+			//synchronized (wp){
 				try {
+					
+					//System.out.println(count+"  22");
+//					this.printPattern(pattern, "get", myCount);
 					wp.wait();
+//					System.out.println(myCount +"  23");
 				} catch (InterruptedException e) {
 					throw new RuntimeException();
 				}
 			}
 		}
-
+		//System.out.println("count="+count+" 31");
 		synchronized (this) {
+//			this.printPattern(pattern, "remove", myCount);
+			//this.printSpace("remove", myCount);
 			space.remove(tp);
+//			this.printSpace("remove", myCount);
 		}
-		
+		//System.out.println("count="+count+" 36");
 		return tp;
 	}
 
 	public String[] read(String... pattern) {
 		String[] tp;
 		String[] wp = addWaiting(pattern);
-		
-		while ((tp = search(pattern)) == null) {
-			synchronized (wp) {
+//		int myCount;
+//		synchronized(this){
+//			count++;
+//			myCount = count;
+//		}
+		synchronized(wp){
+			while ((tp = search(pattern)) == null) {
+			//synchronized (wp) {
 				try {
+					//System.out.println(count+"   64");
+//					this.printPattern(pattern, "read", myCount);
 					wp.wait();
+					//System.out.println(count);
+					//this.printPattern(pattern,);
+//					System.out.println(66);
 				} catch (InterruptedException e) {
 					throw new RuntimeException();
 				}
+			//}
 			}
 		}
-		
 		return tp;
 	}
 
-	public void put(String... tuple) {	
+	public void put(String... tuple) {
+//		int myCount;
 		synchronized (this) {
+//			count++;
+//			myCount = count;
+			//System.out.println(count+"  82");
 			space.add(tuple.clone());
+//			this.printPattern(tuple,"put", myCount);
 		}
 		
 		String[] wp = notifyWaiting(tuple);
-		if (wp != null) wp.notify();
+		if(wp != null){
+			synchronized(wp){
+				wp.notify();
+			}
+		}
 	}
 	
 	private synchronized String[] search(String... pattern) {
@@ -104,7 +151,7 @@ public class LocalTupleSpace implements TupleSpace {
 			if (w.length != tuple.length) continue;
 			boolean found = true;
 			for (int i = 0; i < w.length; i++) {
-				if (w[i] != tuple[i]) {
+				if (w[i] != null && w[i] != tuple[i]) {
 					found = false;
 					break;
 				}
