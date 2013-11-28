37452b4157e0a4ae2edc33f4047866f5a860256b
Yang <yang.deng@aalto.fi>
2013-11-28 16:47:13 +0200

    final tuple space implementation for improving concurrency

--- tuplespace/tuplespaces/LocalTupleSpace.java
+++ tuplespace/tuplespaces/LocalTupleSpace.java
@@ -1,36 +1,36 @@
 package tuplespaces;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
- * This implementation works for the situations where the intersection of any two tuple-sets 
- * that match corresponding patterns is empty.  
+ * Tuple Space implementation. It works this way: when a tuple is put into 
+ * the tuple space, only the thread that waits for that tuple is notified, 
+ * thereby improving the performance of concurrency.
  */
 public class LocalTupleSpace implements TupleSpace {
 	
+	// store all tuples
 	ArrayList<String[]> space;
+	// store all patterns that are used as synchronized objects
 	ArrayList<String[]> waiting;
 	
 	public LocalTupleSpace () {
 		space = new ArrayList<String[]>();
 		waiting = new ArrayList<String[]>();
 	}
-	
+
 	public String[] get(String... pattern) {
 		String[] tp;
-		String[] wp = addWaiting(pattern);
 		
+		String[] wp = addWaiting(pattern);
 		synchronized(wp){
-			while ((tp = search(pattern)) == null) {
+			while ((tp = search(pattern, true)) == null) {
 				try {
 					wp.wait();
 				} catch (InterruptedException e) {
 					System.err.println(e.getMessage());
 				}
-			}
-		
-			synchronized (space) {
-				space.remove(tp);
 			}
 		}
 
@@ -39,10 +39,10 @@ public class LocalTupleSpace implements TupleSpace {
 
 	public String[] read(String... pattern) {
 		String[] tp;
+		
 		String[] wp = addWaiting(pattern);
-
 		synchronized(wp){
-			while ((tp = search(pattern)) == null) {
+			while ((tp = search(pattern, false)) == null) {
 				try {
 					wp.wait();
 				} catch (InterruptedException e) {
@@ -55,6 +55,14 @@ public class LocalTupleSpace implements TupleSpace {
 	}
 
 	public void put(String... tuple) {
+		for (String s : tuple) {
+			if (s == null) {
+				System.err.println("OOps. Tuple " + 
+						Arrays.toString(tuple) + " contains null.");
+				return ;
+			}
+		}
+		
 		synchronized (space) {
 			space.add(tuple.clone());
 		}
@@ -67,18 +75,27 @@ public class LocalTupleSpace implements TupleSpace {
 		}
 	}
 	
-	private String[] search(String... pattern) {
+	/* 
+	 * search tuple that matches pattern in tuple space and remove the matched 
+	 * tuple if toRemove is true.
+	 * We have to make sure that search and remove operations form an atomic 
+	 * operation. Thus, we put remove here.
+	 */
+	private String[] search(String[] pattern, boolean toRemove) {
 		synchronized (space) {
 			for (String[] tp : space) {
 				if (tp.length == pattern.length) {
 					boolean found = true;
 					for (int i = 0; i < pattern.length; i++) {
-						if (pattern[i] != null && pattern[i] != tp[i]) {
+						if (pattern[i] != null && !pattern[i].equals(tp[i])) {
 							found = false;
 							break;
 						}
 					}
-					if (found) return tp;
+					if (found) {
+						if(toRemove) space.remove(tp);
+						return tp;
+					}
 				}
 			}
 		}
@@ -86,6 +103,12 @@ public class LocalTupleSpace implements TupleSpace {
 		return null;
 	}
 	
+	/*
+	 * If there is an existed pattern that is used as a synchronized object
+	 * by some thread, continue using that pattern. If not, then use the new 
+	 * pattern as a synchronized object. Then, the caller of this function 
+	 * might invoke wait() if necessary.
+	 */
 	private String[] addWaiting(String... pattern) {
 		String[] wp = null;
 		
@@ -94,7 +117,8 @@ public class LocalTupleSpace implements TupleSpace {
 				if (w.length != pattern.length) continue;
 				boolean found = true;
 				for (int i = 0; i < pattern.length; i++) {
-					if (pattern[i] != w[i]) {
+					if (!((pattern[i] == null && w[i] == null) ||
+							pattern[i] != null && pattern[i].equals(w[i]))) {
 						found = false;
 						break;
 					}
@@ -114,13 +138,18 @@ public class LocalTupleSpace implements TupleSpace {
 		return wp;
 	}
 	
+	/*
+	 * Find the pattern that matches tuple in waiting, return null if no
+	 * one exists. Then, The caller of this function should invoke notify()
+	 * if pattern exists. 
+	 */
 	private String[] notifyWaiting(String... tuple) {
 		synchronized (waiting) {
 			for (String[] w : waiting) {
 				if (w.length != tuple.length) continue;
 				boolean found = true;
 				for (int i = 0; i < w.length; i++) {
-					if (w[i] != null && w[i] != tuple[i]) {
+					if (w[i] != null && !w[i].equals(tuple[i])) {
 						found = false;
 						break;
 					}
